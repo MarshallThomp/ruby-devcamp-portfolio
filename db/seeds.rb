c0222a0d76c4fe47335eb4bc9fa239ff2598fad6
Marshall <marshall@Marshalls-MacBook-Pro.local>
2019-09-13 08:56:39 -0600

    implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "asdfghjloiuytredfga glaksdjfaklsdjfaksdjh asjdfaklsjd nakljsdakjsdhf  ajsk lakjsdaksjdhfjaksd falksdjflkjas aksjdf  sajdlk aslk jfa;lksdj alksdaksjd aksjdg;jsdgasdklkja;dlkfgj  a;slkdjf asjf laksdjf;lasdjfasdjf  asdfj hhsdafhdsfn lkasdfjkiaeorignalksng oairjgpoiqrjgi0[ojgio"
+    body: "asdfghjloiuytredfga glaksdjfaklsdjfaksdjh asjdfaklsjd nakljsdakjsdhf  ajsk lakjsdaksjdhfjaksd falksdjflkjas aksjdf  sajdlk aslk jfa;lksdj alksdaksjd aksjdg;jsdgasdklkja;dlkfgj  a;slkdjf asjf laksdjf;lasdjfasdjf  asdfj hhsdafhdsfn lkasdfjkiaeorignalksng oairjgpoiqrjgi0[ojgio",
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,10 +25,20 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "My Great Service",
+    subtitle: "Ruby on Rails",
+    body: "asdfghjloiuytredfga glaksdjfaklsdjfaksdjh asjdfaklsjd nakljsdakjsdhf  ajsk lakjsdaksjdhfjaksd falksdjflkjas aksjdf  sajdlk aslk jfa;lksdj alksdaksjd aksjdg;jsdgasdklkja;dlkfgj  a;slkdjf asjf laksdjf;lasdjfasdjf  asdfj hhsdafhdsfn lkasdfjkiaeorignalksng oairjgpoiqrjgi0[ojgio",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
     body: "asdfghjloiuytredfga glaksdjfaklsdjfaksdjh asjdfaklsjd nakljsdakjsdhf  ajsk lakjsdaksjdhfjaksd falksdjflkjas aksjdf  sajdlk aslk jfa;lksdj alksdaksjd aksjdg;jsdgasdklkja;dlkfgj  a;slkdjf asjf laksdjf;lasdjfasdjf  asdfj hhsdafhdsfn lkasdfjkiaeorignalksng oairjgpoiqrjgi0[ojgio",
     main_image: "http://placehold.it/600x400",
     thumb_image: "http://placehold.it/350x200"
