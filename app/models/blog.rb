291ef685eb81ab1207d70c390de56eab08456547
Marshall <marshall@Marshalls-MacBook-Pro.local>
2019-09-17 09:02:18 -0600

    before action for portfolio setting

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -6,4 +6,12 @@ class Blog < ApplicationRecord
   validates_presence_of :title, :body # it will not allow a blog post to be created with out these 2 items field out
 
   belongs_to :topic
+
+  def self.special_blogs
+    all
+  end
+
+  def self.featured_blogs
+    limit(2)
+  end
 end
